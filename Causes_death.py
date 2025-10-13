b5068ef4ad0659826c7ab52ebfacbce29e85cb51
theoguindi-alt <theoguindi@gmail.com>
2025-10-13 17:04:37 -0400

    Columns
    - printing columns (to see titles)
    - converting each column title to arrays
    - printing arrays to see the first few rows     of each selected array value

--- Causes_death.py
+++ Causes_death.py
@@ -11,4 +11,28 @@ import pandas as pd
 
 data = pd.read_csv("NCHS_-_Leading_Causes_of_Death__United_States (3).csv")
 
-print(data)
+
+print(data.columns)
+
+years = data["Year"].to_numpy()
+
+cause_name = data["113 Cause Name"].to_numpy()
+
+cause = data["Cause Name"].to_numpy()
+
+state = data["State"].to_numpy()
+
+deaths = data["Deaths"].to_numpy()
+
+death_rate = data ["Age-adjusted Death Rate"].to_numpy()
+
+
+
+print(years[:3])
+print(cause_name[:5])
+print(cause[:3])
+print(state[:10])
+print(deaths[:3])
+print(death_rate[:15])
+
+
