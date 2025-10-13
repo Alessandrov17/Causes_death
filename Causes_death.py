b7ba3d0532152d4785c17248585e77795b391dac
theoguindi-alt <theoguindi@gmail.com>
2025-10-13 16:35:07 -0400

    import pandas
    - importing pandas module
    - testing print

--- Causes_death.py
+++ Causes_death.py
@@ -6,6 +6,9 @@ The dataset chosen shows the top 10 leading causes of death in the United States
 which are adjusted by age.
 """
 
-import numpy as np 
-Year,Cause_Name,Cause_Name,State,Deaths,Age_adjusted_Death_Rate = np.loadtxt("NCHS_-_Leading_Causes_of_Death__United_States (3).csv",skiprows=1,unpack=True,delimiter=',',usecols=range(0,6))
-  
+
+import pandas as pd
+
+data = pd.read_csv("NCHS_-_Leading_Causes_of_Death__United_States (3).csv")
+
+print(data)
