5ac8ed28816660bc535d03d4117aa9b9db0b5cf7
theoguindi-alt <theoguindi@gmail.com>
2025-10-16 14:01:11 -0400

    Plot 1
    - Bar graph
    - Simplification of entire code
    - 32-36) simplification
    - 26-27) filter the states, so add to get the values for the entire united states entirely
    - 38-43) Make changes to bar plot
    - 26-35) Make (moving lines) changes to bar plot
    - 46-51) Moving lines of certain variable/making color changes to the plot
    - 53-58) removing these because is not useful to us (it was for verifying we loaded the data properly

--- Causes_death.py
+++ Causes_death.py
@@ -8,13 +8,11 @@ which are adjusted by age.
 
 
 import pandas as pd
+import matplotlib.pyplot as plt
 
 data = pd.read_csv("NCHS_-_Leading_Causes_of_Death__United_States (3).csv")
 
-
-
 years = data["Year"].to_numpy()
-
 cause_name = data["113 Cause Name"].to_numpy()
 
 cause = data["Cause Name"].to_numpy()
@@ -29,31 +27,18 @@ death_rate = data ["Age-adjusted Death Rate"].to_numpy()
 #How have age adjusted death rates for the top 10 causes of death 
 #changed from the year 2000 to most recent?
 
-        
-import matplotlib.pyplot as plt
+# Take away the variety in the different states, unites values for the whole united states
+us_data = data[data["State"] == "United States"]
 
-x = years
-y = deaths
+total_deaths_per_year = us_data.groupby("Year")["Deaths"].sum()
 
-plt.xlabel("Years")
-plt.ylabel("Number of Deaths per year")
+plt.bar(total_deaths_per_year.index, total_deaths_per_year.values, color='red', width=0.6)
 
-plt.title("Deaths per year over 1999-2017")
-
-plt.bar(x,y, color = 'r', width = 0.5)
+plt.title("Total Deaths per Year in the United States (1999 - 2017)")
+plt.xlabel("Year")
+plt.ylabel("Total Number of Deaths")
 plt.show()
 
-#This plot looks at the total deaths per year from 1999-2017
 
-ypoints = deaths
 
-plt.plot(ypoints, c = '#4CAF50')
-plt.show()
-
-print(years[:3])
-print(cause_name[:5])
-print(cause[:3])
-print(state[:10])
-print(deaths[:3])
-print(death_rate[:15])
 
