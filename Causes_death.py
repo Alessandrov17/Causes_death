a36e9b6cd890ed8d59ff0180c5e1debeec09b424
theoguindi-alt <theoguindi@gmail.com>
2025-10-16 14:38:18 -0400

    Histogram
    - 17-25) Again simplification of code
    - 43) New histogram plot.
    - 40) sets the graph to a ratio of 8inches by 5 inches
    - 41) bins separates the data into 30 different bars, color is purple, uses plt.hist function to plot graph
    - 43-44) labelling of graphs

--- Causes_death.py
+++ Causes_death.py
@@ -6,7 +6,6 @@ The dataset chosen shows the top 10 leading causes of death in the United States
 which are adjusted by age.
 """
 
-
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -14,15 +13,12 @@ data = pd.read_csv("NCHS_-_Leading_Causes_of_Death__United_States (3).csv")
 
 years = data["Year"].to_numpy()
 cause_name = data["113 Cause Name"].to_numpy()
-
 cause = data["Cause Name"].to_numpy()
-
 state = data["State"].to_numpy()
-
 deaths = data["Deaths"].to_numpy()
+death_rate = data["Age-adjusted Death Rate"].to_numpy()
 
-death_rate = data ["Age-adjusted Death Rate"].to_numpy()
-
+# 1) This is a bar graph showcasing the deaths per year from the year 1999-2017
 
 #How have age adjusted death rates for the top 10 causes of death 
 #changed from the year 2000 to most recent?
@@ -40,5 +36,11 @@ plt.ylabel("Total Number of Deaths")
 plt.show()
 
 
-
+# 2) Histogram with age-adjusted death rates for 1999-2017
+plt.figure(figsize=(8,5))
+plt.hist(us_data["Age-adjusted Death Rate"], bins=30, color='purple')
+plt.title("Distribution of Age-adjusted Death Rates (1999 - 2017)")
+plt.xlabel("Age-adjusted Death Rate (per 100,000 population)")
+plt.ylabel("Frequency")
+plt.show()
 
