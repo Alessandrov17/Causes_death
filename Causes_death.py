8dcaa9214b341da50e30f75aed2f364da0f4b169
theoguindi-alt <theoguindi@gmail.com>
2025-10-16 11:53:06 -0400

    Graph
    -29-31Attempt to answer question 3 from LIA requirements
    -31-39 inputting graph using myplotlib.pyplot module, using values for years and deaths
    graph title.
    -43-44 adding extra parameters to the bar, the color red and the width 0.5
    -46-51 another graph with only y-coordinates

--- Causes_death.py
+++ Causes_death.py
@@ -26,14 +26,29 @@ deaths = data["Deaths"].to_numpy()
 death_rate = data ["Age-adjusted Death Rate"].to_numpy()
 
 
-#filtering all the cases for the state of arkansas
+#How have age adjusted death rates for the top 10 causes of death 
+#changed from the year 2000 to most recent?
+
         
-for i in range(len(state)):
-    if state[i] == "Arkansas":
-        print("Year:", years[i], "Cause:", cause_name[i], "Deaths:", deaths[i])
+import matplotlib.pyplot as plt
 
+x = years
+y = deaths
 
+plt.xlabel("Years")
+plt.ylabel("Number of Deaths per year")
 
+plt.title("Deaths per year over 1999-2017")
+
+plt.bar(x,y, color = 'r', width = 0.5)
+plt.show()
+
+#This plot looks at the total deaths per year from 1999-2017
+
+ypoints = deaths
+
+plt.plot(ypoints, c = '#4CAF50')
+plt.show()
 
 print(years[:3])
 print(cause_name[:5])
@@ -41,5 +56,4 @@ print(cause[:3])
 print(state[:10])
 print(deaths[:3])
 print(death_rate[:15])
-
 
